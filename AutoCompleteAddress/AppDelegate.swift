8c7b863bc36298071b7657dd1dd2cc1dc0acdce3
Agus Cahyono <cahyo.mamen@gmail.com>
2024-12-10 14:29:35 +0700

    Update AppDelegate.swift

--- AutoCompleteAddress/AppDelegate.swift
+++ AutoCompleteAddress/AppDelegate.swift
@@ -17,11 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        //GMSServices.provideAPIKey("AIzaSyCYluA2nlkvz71ynh1VBj3vs1gPZTq3oWs")
-        //GMSPlacesClient.provideAPIKey("AIzaSyCYluA2nlkvz71ynh1VBj3vs1gPZTq3oWs")
         GMSServices.provideAPIKey("enter your API Key")
-        
         GMSPlacesClient.provideAPIKey("enter your API Key")
         return true
     }
